927354c43f2bfe87c2cda1f4111666ecac1ae9c9
Alessia <80739359+Alerian@users.noreply.github.com>
2023-02-03 23:30:32 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Navbar } from "./components/Navbar";
+
+export const App = () => {
+  return (
+    <section className="page-body">
+      {/* Navigation Bar */}
+      <div className="navbar-body">
+        <Navbar />
+      </div>
+    </section>
+  );
+};
